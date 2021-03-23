531196e120ed92ac1d2f4374ec033dee4eea7e73
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-03-23 21:03:49 +0900

    MP : Week 3 Training 2

---
+++ MobileProgramming/Week_3/Training2/app/src/main/java/com/example/week_3_2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.week_3_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+    fun init(){
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.radioButton1 -> imageView.setImageResource((R.drawable.welsh_3))
+                R.id.radioButton2 -> imageView.setImageResource((R.drawable.welsh_2))
+                R.id.radioButton3 -> imageView.setImageResource((R.drawable.welsh_1))
+            }
+        }
+    }
+}
