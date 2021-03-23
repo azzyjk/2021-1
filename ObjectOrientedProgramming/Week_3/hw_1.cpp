44b0a1e88ecfbab7c7962aefd7e5b91549724af1
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-03-23 21:00:20 +0900

    OOP : Week 3 Training

---
+++ ObjectOrientedProgramming/Week_3/hw_1.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+#include<fstream>
+
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new (_NORMAL_BLOCK, __FILE__, __LINE__)
+#define new DBG_NEW
+#endif
+#endif
+
+using namespace std;
+
+int** loadMap(int& raw, int& col) {
+	ifstream fin("./Week_3/Homework/map.txt");
+	if (!fin.is_open()) {
+		cerr << "File is not open!!" << endl;
+		exit(0);
+	}
+
+	fin >> raw >> col;
+
+	int* (*arr) = new int* [raw];
+
+	for (int i = 0; i < raw; i++) {
+		arr[i] = new int[col];
+	}
+
+	for (int i = 0; i < raw; i++) {
+		for (int j = 0; j < col; j++) {
+			fin >> arr[i][j];
+		}
+	}
+	fin.close();
+	return arr;
+}
+
+void printMap(int** arr, int raw, int col) {
+	for (int i = 0; i < raw; i++) {
+		for (int j = 0; j < col; j++) {
+			cout << (arr[i][j] ? "●" : "○") << " ";
+		}
+		cout << endl;
+	}
+}
+
+void removeMap(int** (&arr), int raw, int col) {
+	for (int i = 0; i < raw; i++) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+	arr = nullptr;
+}
+
+int main() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	cout << "201711425 정준원" << endl;
+	
+	int raw, col;
+	int** arr = nullptr;
+
+	arr = loadMap(raw, col);
+
+	if (arr != nullptr) {
+		printMap(arr, raw, col);
+		removeMap(arr, raw, col);
+	}
+
+	return 0;
+}
