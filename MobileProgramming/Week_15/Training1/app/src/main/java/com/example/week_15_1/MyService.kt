c188548d6c573686a79b621cbc733467bab5a7d5
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-07-15 10:38:39 +0900

    MP : Week_15 Training

---
+++ MobileProgramming/Week_15/Training1/app/src/main/java/com/example/week_15_1/MyService.kt
@@ -0,0 +1,50 @@
+package com.example.week_15_1
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyService : Service() {
+    var thread:Thread ?= null
+    var num = 0
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.i("MyService", "onCreate()")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i("MyService", "onStartCommand()")
+        if(thread==null){
+            thread = object:Thread("MyThread"){
+                override fun run() {
+                    try{
+                        for(i in 0..10){
+                            Log.i("MyThread $num", "Count : $i")
+                            Thread.sleep(1000)
+                        }
+                    }catch(e:InterruptedException){
+                        Thread.currentThread().interrupt()
+                    }
+                    thread = null
+                }
+            }
+        }
+        thread!!.start()
+        num++
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(thread!=null)
+            thread!!.interrupt()
+        thread = null
+        Log.i("MyService", "onDestroy()")
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+}
